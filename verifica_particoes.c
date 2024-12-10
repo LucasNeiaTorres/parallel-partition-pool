314ce4aadd31761fc3ff05a52550e68ff4730658
Lucas Néia <lucasneia07@gmail.com>
2024-12-09 22:26:47 -0300

    arruma verifica particoes

--- verifica_particoes.c
+++ verifica_particoes.c
@@ -1,55 +1,33 @@
 // Autores: Lucas Néia Torres, Inaiê Moscal Zambonin
 
 #include <stdio.h>
-#include "verifica_particoes.h"
+#include <limits.h>
+#include <stdlib.h>
 
-void verifica_particoes(long long *Input, int n, long long *P, int np, long long *Output, int *Pos)
-{
-    int i, j;
+void verifica_particoes(long long *Input, int n, long long *P, int np, long long *Output, int *Pos) {
+    int erro = 0;
+    long long faixa_min = 0;
+    long long faixa_max;
 
-    for (i = 0; i < np; i++)
-    {
-        // se a partição i começa depois do final do vetor de entrada, então o particionamento está errado
-        if (Pos[i] > n)
-        {
-            printf("particionamento COM ERROS\n");
-            printf("1 - Pos[%d] = %d > %d\n", i, Pos[i], n);
-            return;
-        }
-
-        // teste para não acessar posição inválida
-        if (i + 1 < np)
-        {
-            // se a partição i+1 começa antes da partição i, então o particionamento está errado
-            if (Pos[i] > Pos[i + 1])
-            {
-                printf("particionamento COM ERROS\n");
-                printf("2 - Pos[%d] = %d > Pos[%d] = %d\n", i, Pos[i], i + 1, Pos[i + 1]);
-                return;
-            }
-            for (j = Pos[i]; j < Pos[i + 1]; j++)
-            {
-                if (Output[j] >= P[i])
-                {
-                    printf("particionamento COM ERROS\n");
-                    printf("3 - Output[%d] = %lld >= P[%d] = %lld\n", j, Output[j], i, P[i]);
-                    return;
-                }
+    // Verificar as partições uma a uma
+    for (int i = 0; i < np; i++) {
+        faixa_max = P[i];
+        
+        for (int j = Pos[i]; j < ((i == np - 1) ? n : Pos[i + 1]); j++) {
+            if (Output[j] < faixa_min || Output[j] >= faixa_max) {
+                erro = 1;
+                // printf("Erro na faixa %d: elemento %lld fora do intervalo [%lld, %lld)\n", 
+                    //    i, Output[j], faixa_min, faixa_max);
             }
         }
-        else
-        {
-            // caso seja ultima partição, for não pode ir até Pos[i + 1] pois não existe
-            for (j = Pos[i]; j < n; j++)
-            {
-                if (Output[j] >= P[i])
-                {
-                    printf("particionamento COM ERROS\n");
-                    printf("4 - Output[%d] = %lld >= P[%d] = %lld\n", j, Output[j], i, P[i]);
-                    return;
-                }
-            }
-        }
+
+        faixa_min = faixa_max;
     }
-    printf("particionamento CORRETO\n");
+
+    // Exibir resultado final
+    if (erro) {
+        printf("===> particionamento COM ERROS\n");
+    } else {
+        printf("===> particionamento CORRETO\n");
+    }
 }
